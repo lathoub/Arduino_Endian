f75e7178fd022a66867add4f24f0fb51acdda740
lathoub <bart.de.lathouwer@telenet.be>
2019-10-20 15:23:19 +0200

    added swapping functions

--- examples/BigOrLittle/BigOrLittle.ino
+++ examples/BigOrLittle/BigOrLittle.ino
@@ -1,10 +1,19 @@
 #include <Arduino.h>
-#include <endian.h>
+#include "endian.h"
+
+int aa(int v) {
+  return v + 1;
+}
+
+#define bb(x) aa(x)
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("booting..");
+
+  Serial.println(aa(5));
+  Serial.println(bb(5));
 
 #ifndef BYTE_ORDER
   Serial.println("BYTE_ORDER not defined");
@@ -16,6 +25,17 @@ void setup()
   Serial.println("Big Endian");
 #endif
 #endif
+
+  conversionBuffer g;
+
+  g.buffer[0] = 0x0A;
+  g.buffer[1] = 0x0B;
+  g.buffer[2] = 0x0C;  
+  g.buffer[3] = 0x0D;
+  Serial.println(g.value8 , HEX);
+  Serial.println(g.value16, HEX);
+  Serial.println(g.value32, HEX);
+  Serial.println(htobe32(g.value32), HEX);
 
   // from https://en.wikipedia.org/wiki/Endianness
   uint32_t word = 0x0A0B0C0D; // An unsigned 32-bit integer.
